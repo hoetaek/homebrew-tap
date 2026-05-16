bf2256e44cfdfbbd733c06e9254322cf90c1d406
hoetaek <rhoetaek@gmail.com>
2026-05-16 18:19:21 +0900

    wt 0.19.0

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -1,14 +1,58 @@
 class Wt < Formula
   desc "Git worktree workspace manager"
   homepage "https://github.com/hoetaek/wt"
-  url "https://github.com/hoetaek/wt/archive/refs/tags/v0.18.0.tar.gz"
-  sha256 "220255b86408cc9bef48f46e16543baf4c3eb822f5ccc48cea5347ac72da3cd9"
+  version "0.19.0"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/hoetaek/wt/releases/download/v0.19.0/wt-aarch64-apple-darwin.tar.xz"
+      sha256 "b9394eeca6a2064d642368d3464c136149ac58948313081e5acb8630d729d0e1"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/hoetaek/wt/releases/download/v0.19.0/wt-x86_64-apple-darwin.tar.xz"
+      sha256 "2a5258f7bb80335eecd14bad43b0641bf8eacf6a12cffb228bcabb3d4cb45fa4"
+    end
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/hoetaek/wt/releases/download/v0.19.0/wt-x86_64-unknown-linux-gnu.tar.xz"
+    sha256 "82f13817d627ebd84e7c2bb5dcd764a20cd9bfd14bb269c9bc1ba362a6ff0c22"
+  end
   license any_of: ["MIT", "Apache-2.0"]
 
-  depends_on "rust" => :build
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin":     {},
+    "x86_64-apple-darwin":      {},
+    "x86_64-unknown-linux-gnu": {},
+  }.freeze
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "wt" if OS.mac? && Hardware::CPU.arm?
+    bin.install "wt" if OS.mac? && Hardware::CPU.intel?
+    bin.install "wt" if OS.linux? && Hardware::CPU.intel?
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
   end
 
   test do
